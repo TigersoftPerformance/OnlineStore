28488aee1cb5a07be62a69086ec4380f481d1dbb
john <john@john-Qosmio-X500>
2014-07-04 22:18:50 +1000

    Initial Commit

---
+++ DB Data/DumpDB.sh
@@ -0,0 +1,15 @@
+sudo rm -f /tmp/Cars* /tmp/ModelCodes* /tmp/Superchips* /tmp/Categories* /tmp/BMC* /tmp/CarFilters* /tmp/BCRacing* /tmp/FI.* /tmp/QuantumCars.*
+
+sudo -u john mysqldump --tab="/tmp" --fields-enclosed-by='"' --fields-terminated-by="," --lines-terminated-by="\n" --no-create-info TP;
+cp /tmp/Cars.txt ./Cars.csv
+cp /tmp/ModelCodes.txt ./ModelCodes.csv
+cp /tmp/SuperchipsWebsite.txt ./SuperchipsWebsite.csv
+cp /tmp/SuperchipsMakes.txt ./SuperchipsMakes.csv
+cp /tmp/Categories.txt ./Categories.csv
+cp /tmp/BMCAirFilters.txt ./BMCAirFilters.csv
+cp /tmp/BMCCars.txt ./BMCCars.csv
+cp /tmp/BMCmods.txt ./BMCmods.csv
+cp /tmp/CarFilters.txt ./CarFilters.csv
+cp /tmp/BCRacingCoilovers.txt ./BCRacingCoilovers.csv
+cp /tmp/FI.txt ./FI.csv
+cp /tmp/QuantumCars.txt ./QuantumCars.csv
