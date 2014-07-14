06700644703c7945f9d382fb13a5dd06dd7cfcb7
john <john@john-Qosmio-X500>
2014-07-14 23:59:12 +1000

    First draft of DBCheck.pl

--- DBCheck.pl
+++ DBCheck.pl
@@ -25,6 +25,45 @@ my $dbh = DBI->connect($dsn, undef, undef,
 # prepare the sql statements
 # This is for the main loop, we will read every entry with a model code
 my $carsth = $dbh->prepare("
-	SELECT * FROM Cars WHERE model_code != ''
+	SELECT * FROM Cars WHERE active='Y'
 ") or die $dbh->errstr;
 $carsth->execute() or die $dbh->errstr;
+
+# In this section, we will do Sanity checks on the cars table only, including:
+# Make sure that the Dates are in order
+my $car_data = {};
+while ($car_data = $carsth->fetchrow_hashref)
+	{
+	my $car_start_year = substr ($car_data->{start_date}, 0, 4);
+	my $car_end_year = substr ($car_data->{end_date}, 0, 4);
+	
+	if ($car_start_year < $car_end_year)
+		{
+		next;
+		}
+	if ($car_start_year == $car_end_year)
+		{
+		my $car_start_month = substr ($car_data->{start_date}, 5, 2);
+		my $car_end_month = substr ($car_data->{end_date}, 5, 2);
+		
+		if ($car_start_month <= $car_end_month)
+			{
+			next;
+			}
+		}
+	say "Dates wrong for car $car_data->{idCars} - $car_data->{make} - $car_data->{model} - $car_data->{variant}"; 
+	}
+
+
+# In this section, we compare data in the Cars Table, to that in the SuperchipsWebsite table, including:
+# Are the dates the same?
+# Is the engine capacity the same?
+# Are the engine power and torque figures the same?
+# Are there Superchips tunes that do not have an entry in the cars table?
+# Are there Cars in the Cars table pointing to a Superchips tune that does not exist?
+
+
+
+
+# In this section, we compare the Cars table to the ModelCodes tables
+
