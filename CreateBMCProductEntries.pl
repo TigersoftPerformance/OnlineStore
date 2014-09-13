7a11188c70e717ec83396035798690db3ba3c87f
John <john@tigersoft.com.au>
2014-09-14 00:32:47 +1000

    Improved Logging

--- CreateBMCProductEntries.pl
+++ CreateBMCProductEntries.pl
@@ -6,8 +6,17 @@ use DBI;
 use English;
 use feature 'say';
 
-use constant LOG => "./log_bmc_entries.TXT";
-open (my $logfh, ">", LOG) or die "cannot open " . LOG; 
+my $url = 'http://au.bmcairfilters.com';
+use constant LOGFILE    => "./Logs/MatchCars.log";
+use constant ALERTFILE  => "./Logs/MatchCars.alert";
+use constant DEBUGFILE  => "./Logs/MatchCars.debug";
+
+open(my $logfh, ">", LOGFILE)     or die "cannot open LOGFILE $!";
+open(my $alertfh, ">", ALERTFILE) or die "cannot open ALERTFILE $!";
+open(my $debugfh, ">", DEBUGFILE) or die "cannot open DEBUGFILE $!";
+$logfh->autoflush;
+$alertfh->autoflush;
+$debugfh->autoflush;
 
 use constant SPECIAL_PRICE => 0.85;
 use constant SPECIAL_START => "2014-06-01 00:00:00";
@@ -35,26 +44,35 @@ my $dbh = DBI->connect($dsn, undef, undef,
 ) or die $DBI::errstr;
 
 #
-# Select a row from BMCAirFilters based on part_id
+# Select all rows from BMCproducts
 #
 my $get_bmc_sth = $dbh->prepare("
-	SELECT * FROM BMCAirProducts WHERE active = 'Y'
+	SELECT * FROM BMCProducts WHERE active = 'Y'
 ") or die $dbh->errstr;
 
 $get_bmc_sth->execute;
 
 #
-# Select a row from Categories based on idCars
+# Select a row from BMCStockedProducts based on bmc_part_id
+#
+my $get_bmcsp_sth = $dbh->prepare("
+	SELECT * FROM BMCStockedProducts WHERE bmc_part_id = ?
+") or die $dbh->errstr;
+
+
+
+#
+# Select a row from Categories based on partid
 #
 my $get_cat_sth = $dbh->prepare("
-	SELECT * FROM Categories WHERE idCars = ? AND active = 'Y'
+	SELECT * FROM Categories WHERE partid = ? AND active = 'Y'
 ") or die $dbh->errstr;
 
 #
 # Update/Insert row into ZenCartStoreEntries table
 #
 my $insertth = $dbh->prepare("
-	INSERT INTO ZenCartStoreEntries VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE v_status='1'
+	REPLACE INTO ZenCartStoreEntries VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) 
 ") or die $dbh->errstr;
 
 
@@ -89,7 +107,7 @@ my $insertth = $dbh->prepare("
 # $18 =	v_date_avail,
 	my $v_date_avail = "0000-00-00 00:00:00";
 # $19 =	v_date_added,
-	my $v_date_added = "2013-12-27 00:25:30";
+	my $v_date_added = "2014-09-13 00:00:00";
 # $20 =	v_products_quantity,
 	my $v_products_quantity = 100;
 # $21 =	v_manufacturers_name,
@@ -97,7 +115,7 @@ my $insertth = $dbh->prepare("
 # $22 =	v_categories_name_1,
 
 # $23 =	v_tax_class_title,
-	my $v_tax_class_title = "--none--";
+	my $v_tax_class_title = "Taxable Goods";
 # $24 =	v_status,
 	my $v_status = 1;
 # $25 =	v_metatags_products_name_status,
@@ -117,14 +135,13 @@ my $insertth = $dbh->prepare("
 # $32 =	v_metatags_description_1
 	my $v_metatags_description_1 = "";
 
-	my $no_tune = 1;
 	my $v_products_price = 0;
 	my $v_products_image = "";
 	my $v_products_model = "";
 	my $v_products_name_1 = "";
 	my $v_products_description_1 = "";
 	my $v_categories_name_1;
-	my $id = 0;
+
 
 
 #
@@ -132,29 +149,41 @@ my $insertth = $dbh->prepare("
 # This main loop reads each row from the Cars table
 #
 my $bmc_data = {};
-#my bmcpartid = {};
 while ($bmc_data = $get_bmc_sth->fetchrow_hashref)
 	{
-
 	$sortorder += SORT_ORDER_INCREMENT;
-	$v_categories_name_1 = $bmc_data->{cat};
 
-	my @cat_elements = split('\^',$bmc_data->{cat});
+	$get_cat_sth->execute ($bmc_data->{type}) or die $dbh->errstr;
+	my $category = $get_cat_sth->fetchrow_hashref;
+	if (!defined $category->{longname})
+		{
+		&screen ("ERROR: Could not find category for $bmc_data->{type}");
+		next;
+		}
+	$v_categories_name_1 = $category->{longname};
 
-			$v_products_price = $bmc_data->{RRP} || 0;
-			$v_specials_price = $v_products_price * SPECIAL_PRICE;
-			$v_products_image = ($bmc_data->{image} =~ /.*\/(.*)$/i ? $1 : "" ) if $bmc_data->{image};
+	$get_bmcsp_sth->execute($bmc_data->{bmc_part_id}) or die $dbh->errstr;
+	my $bmcsp = $get_bmcsp_sth->fetchrow_hashref;
+	
+	if (!defined $bmcsp->{tp_price})
+		{
+		&log ("WARNING: Could not find Stocked Products for $bmc_data->{bmc_part_id}");
+		next;
+		}
+	$v_products_price = $bmcsp->{tp_price} - ($bmcsp->{tp_price} / 11);
+	$v_specials_price = $v_products_price * SPECIAL_PRICE;
+	$v_products_image = ($bmc_data->{image} =~ /.*\/(.*)$/i ? $1 : "" ) if $bmc_data->{image};
 
 
-			##############################
-			$v_products_model = $cat_elements[1] . $id++;
-			$v_products_name_1 = $cat_elements[-1];
-			$v_products_description_1 = description("BMC Products",$bmc_data->{part},$bmc_data->{description},$bmc_data->{dimname1},$bmc_data->{dimvalue1},$bmc_data->{dimname2},$bmc_data->{dimvalue2},$bmc_data->{dimname3},$bmc_data->{dimvalue3},$bmc_data->{image},$bmc_data->{diagram},$v_products_model);
-			$v_metatags_title_1 = $v_products_name_1;
-			$v_metatags_keywords_1 = $v_products_name_1;
-			$v_metatags_description_1 = $v_products_description_1;
-			&insert_store_entry ();
-			print $logfh "CR $bmc_data->{part} ==>";
+	##############################
+	$v_products_model = $bmc_data->{bmc_part_id};
+	$v_products_name_1 = $bmc_data->{bmc_part_id};
+	$v_products_description_1 = description("BMC Products",$bmc_data->{bmc_part_id},$bmc_data->{description},$bmc_data->{dimname1},$bmc_data->{dimvalue1},$bmc_data->{dimname2},$bmc_data->{dimvalue2},$bmc_data->{dimname3},$bmc_data->{dimvalue3},$bmc_data->{image},$bmc_data->{diagram},$v_products_model);
+	$v_metatags_title_1 = $v_products_name_1;
+	$v_metatags_keywords_1 = $v_products_name_1;
+	$v_metatags_description_1 = $v_products_name_1;
+	&insert_store_entry ();
+	&debug ("CR $bmc_data->{bmc_part_id} ==>");
 
 	}
 
@@ -189,7 +218,7 @@ $dimvalue2 ||="";
 $dimvalue3 ||="";
 $description ||="";
 $description =~ s/class=\"red\"/style=\"color:red\"/g;
-print $logfh "$filter_type,$part,$dimname1,$dimvalue1,$dimname2,$dimvalue2,$dimname3,$dimvalue3,$image,$diagram,$products_model\n";
+&debug ("$filter_type,$part,$dimname1,$dimvalue1,$dimname2,$dimvalue2,$dimname3,$dimvalue3,$image,$diagram,$products_model\n");
 my $products_description = sprintf("<table style=\"margin:0 auto&#59\"><thead><tr><th colspan=\"3\" style=\"font-family: Impact&#44 Arial&#44 sans-serif&#59font-size: 1.40em&#59font-weight: normal&#59padding: 8px 8px 8px 16px&#59\"> %s </th></tr></thead><tbody><tr>", $filter_type ." - ".$part);
    $products_description .= sprintf("<td width=\"130\" valign=\"top\"><span><font color=\"red\"><b>Code:</b></font></span> %s <br><br><span><font color=\"red\"><b>%s </b></font></span> %s <br><span><font color=\"red\"><b>%s </b></font></span> %s <br><span><font color=\"red\"><b>%s </b></font></span> %s <br></td><td>", $part, ($dimname1 ? "$dimname1:" : ""),$dimvalue1,($dimname2 ? "$dimname2:" : ""),$dimvalue2,($dimname3 ? "$dimname3:" : ""),$dimvalue3);
    $products_description .= sprintf("<img style=\"max-width:200px\" src=\"http://au.bmcairfilters.com/%s\" alt=\"%s\"><br><br></td>",$image,$products_model);
@@ -200,3 +229,31 @@ my $products_description = sprintf("<table style=\"margin:0 auto&#59\"><thead><t
    $products_description =~ s/\;//g;
 return $products_description;
 }
+
+
+sub screen
+	{
+	my $line = shift;
+	say $line;
+	&alert ($line);
+	}
+	
+sub alert
+	{
+	my $line = shift;
+	say $alertfh $line;
+	&log ($line);
+	}
+	
+sub log
+	{
+	my $line = shift;
+	say $logfh $line;
+	&debug ($line);
+	}
+
+sub debug
+	{
+	my $line = shift;
+	say $debugfh $line;
+	}
